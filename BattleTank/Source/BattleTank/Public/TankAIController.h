d376cba0c0cb09e84b9f7ba27f0727dd020f1857
Blysku <blyskx@gmail.com>
2017-12-28 12:07:14 +0100

    BluprintCallable()

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -9,19 +9,19 @@
 #include "TankAIController.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
-	
-private:	
+
+private:
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
 
-	ATank * GetControlledTank() const;
+	ATank* GetControlledTank() const;
 
 	ATank* GetPlayerTank() const;
 
