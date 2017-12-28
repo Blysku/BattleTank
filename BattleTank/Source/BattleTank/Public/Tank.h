8fd1cd706e61575ee5060c3f560e3223ab95204c
Blysku <blyskx@gmail.com>
2017-12-28 11:38:06 +0100

    Create Default Sub Objects in C++

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -13,6 +14,9 @@ class BATTLETANK_API ATank : public APawn
 
 public:
 	void AimAt(FVector HitLocation);
+
+protected:
+	UTankAimingComponent * TankAimingComponent = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
@@ -27,6 +31,4 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	
-	
 };
